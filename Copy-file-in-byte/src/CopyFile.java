88bd32082075484a89583c65bc4a26a422d03d15
Oliver King <oliver.csu@gmail.com>
2018-07-28 18:50:48 +0800

    rebuilt in array

--- Copy-file-in-byte/src/CopyFile.java
+++ Copy-file-in-byte/src/CopyFile.java
@@ -1,29 +1,25 @@
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class CopyFile {
 
 	public static void main(String[] args) throws IOException {
-		// 1,明确源和目的。
 		File srcFile = new File("c:\\YesDir\test.JPG");
 		File destFile = new File("copyTest.JPG");
-
-		// 2,明确字节流 输入流和源相关联，输出流和目的关联。
+		// 明确字节流 输入流和源相关联，输出流和目的关联。
 		FileInputStream fis = new FileInputStream(srcFile);
 		FileOutputStream fos = new FileOutputStream(destFile);
-
-		// 3, 使用输入流的读取方法读取字节，并将字节写入到目的中。
-		int ch = 0;
-		while ((ch = fis.read()) != -1) {
-			fos.write(ch);
+		//定义一个缓冲区。
+		byte[] buf = new byte[1024];
+		int len = 0;
+		while ((len = fis.read(buf)) != -1) {
+			fos.write(buf, 0, len);// 将数组中的指定长度的数据写入到输出流中。
 		}
-		// 4,关闭资源。
+		// 关闭资源。
 		fos.close();
 		fis.close();
-
 	}
 
 }
